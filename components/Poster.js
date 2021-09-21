45a25c7e723461a6a9c06f29c7d3ad59028b933b
apekshajain05 <62800258+apekshajain05@users.noreply.github.com>
2021-09-21 14:30:37 +0530

    Add files via upload

--- components/Poster.js
+++ components/Poster.js
@@ -18,7 +18,7 @@ AFRAME.registerComponent('posters',{
             {
                 id:'spiderman',
                 title:'Spiderman',
-                url:'spiderman.jpg'
+                url:'./assets/posters/spiderman.jpg'
             },
             {
                 id:'superman',
@@ -27,58 +27,56 @@ AFRAME.registerComponent('posters',{
             }
         ];
 
-        let previousXpos=-73
+        let previousXPosition=-60
+        for(var item of thumbnailRef){
+          const posX=previousXPosition+25;
+          const posY=12;
+          const posZ=-40;
+          const position={x:posX,y:posY,z:posZ}
+          previousXPosition=posX
 
-        for(var item in thumbnailRef){
-            const posX=previousXpos+30
-            const posY=10
-            const posZ=-40
-            const position={x:posX,y:posY,z:posZ}
-            previousXpos=posX
+          const borderEl=this.createBorder(position,item.id)
+          const thumbnail=this.createThumbnail(item)
+          borderEl.appendChild(thumbnail)
 
-            const borderEl=this.createBorder(position,item.id)
-            const thumbnail=this.createThumbnail(item)
-            borderEl.appendChild(thumbnail)
-            
-            
-
-            this.posterContainer.appendChild(borderEl)
-            
+          this.posterContainer.appendChild(borderEl)
         }
-    },
+            
+        },
+    
 
     createBorder:function(position,id){
-        const entityEl=document.createElement('a-entity')
-        entityEl.setAttribute('id',id)
-        entityEl.setAttribute('visible',true)
-        entityEl.setAttribute('geometry',{
-          primitive:'ring',
-          radiusInner:9,
-          radiusOuter:10
-        })
-        entityEl.setAttribute('position',position)
-        entityEl.setAttribute('material',{
-          color:'#292c52',
-          opacity:0.8,
-        })
-    
-        return entityEl
-      },
-    
-      createThumbnail:function(item){
-        const entityEl=document.createElement('a-entity')
-        entityEl.setAttribute('visible',true)
-        entityEl.setAttribute('geometry',{
-          primitive:'circle',
-          radius:9,
-        })
-        entityEl.setAttribute('material',{
-          src:item.url
-        })
-    
-        return entityEl
-      },
-    
+      const entityEl=document.createElement('a-entity')
+      entityEl.setAttribute('id',id)
+      entityEl.setAttribute('visible',true)
+      entityEl.setAttribute('geometry',{
+        primitive:'ring',
+        radiusInner:10,
+        radiusOuter:11
+      })
+      entityEl.setAttribute('position',position)
+      entityEl.setAttribute('material',{
+        color:'#292c52',
+        opacity:0.8,
+      })
+  
+      return entityEl
+    },
+  
+    createThumbnail:function(item){
+      const entityEl=document.createElement('a-entity')
+      entityEl.setAttribute('visible',true)
+      entityEl.setAttribute('geometry',{
+        primitive:'circle',
+        radius:10,
+      })
+      entityEl.setAttribute('scale',{x:1,y:1,z:1})
+      entityEl.setAttribute('material',{
+        src:item.url
+      })
+  
+      return entityEl
+    },
     
     
       
